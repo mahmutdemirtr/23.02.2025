5e0437f4f81929da02f3bd5c69805afdd7f84781
mahmutdemirtr <140492186+mahmutdemirtr@users.noreply.github.com>
2025-02-23 17:06:34 +0300

    add the  backend

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,30 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  bundlePagesRouterDependencies: true,
-};
+// next.config.js
+module.exports = {
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+        port: "",
+        pathname: '/**'
+      },
+    ],
+  },
+  webpack: (config) => {
+    config.resolve.fallback = { 
+      fs: false,
+      path: false, 
+    };
 
-export default nextConfig;
+    return config;
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://ec2-100-27-214-198.compute-1.amazonaws.com:8000/api/:path*",
+      },
+    ];
+  },
+};
